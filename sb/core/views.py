4916edc89c8716163f7b97bf7aeb74de2833ed38
atiq012 <atiqrshakil@gmail.com>
2021-09-26 18:48:24 +0600

    multiple newspaper

--- sb/core/views.py
+++ sb/core/views.py
@@ -14,16 +14,16 @@ def get_html_content(keywords):
     session.headers['Content-Language'] = LANGUAGE
     if keywords:
         keywords = keywords.replace(' ','%20')
+
         html_content = session.get(f'https://www.prothomalo.com/search?q={keywords}').text
-        # print(html_content)
-        
+         
     else:
         keywords = ''
-        html_content = ''        
+        html_content = ''
     
     return html_content
 
-# new age paper
+# the financial express
 def get_html_content_new_age(keywords):
     import requests
     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
@@ -41,51 +41,37 @@ def get_html_content_new_age(keywords):
         html_content_newage = ''        
     
     return html_content_newage
-
     
 def home(request):
+    # import datetime
+    # stime = "09/21/2021 00:00:00.0000"
+    
+    # print(int(datetime.datetime.strptime(stime, '%m/%d/%Y %H:%M:%S.%f').timestamp()))
+    # 1632160800
+    # 
     result = None
     divs = None
     linkAndHeader = None
-    # linkAndHeaderNews = None
+    linkAndHeader2 = None
+    
     if 'keywords' in request.GET:
         
         keywords = request.GET.get('keywords')
         # protom alo
         html_content = get_html_content(keywords)
         
-        # new age
+        # financial express
         html_content_new_age = get_html_content_new_age(keywords)
         
         import re
         from bs4 import BeautifulSoup
 
         soup = BeautifulSoup(html_content, 'html.parser')
-        # newsage
-        soupForNewAge = BeautifulSoup(html_content_new_age,'html.parser')
         
-        # single data
-        # if keywords:
-        #     result = dict()
-            
-        #     linkValue = soup.find('a', attrs={'href': re.compile("^https://")})
-            
-        #     result['link'] = str(linkValue.attrs['href']) + "\n"
-        #     # result['link'] = soup.find()
-
-        #     result['title'] = soup.find('h2', class_ = 'headline').text
-        # else:
-        #     result = None      
-
-
-
-        # all datas
+        
+        # prothom alo all datas
         divs = soup.find_all('div', class_ = 'customStoryCard9-m__base__1rOCp')
         
-        # print(divs)
-        # print (divsNewAge)
-        # for items in divs:
-        #     result = items.find('h2', class_ = 'headline').text
         allLinks = []
         allHeader = []
 
@@ -99,12 +85,26 @@ def home(request):
             
         # We have two list. So, we will zip them. 
         # First putting header in the zip then link.
-        linkAndHeader = zip(allHeader, allLinks) 
-
+        linkAndHeader = zip(allHeader, allLinks)
         
+        # financial express
+        soupForNewAge = BeautifulSoup(html_content_new_age,'html.parser')
+        financialExp = soupForNewAge.find_all('div', class_="card row")
+
+        allLinksFinanceExp = []
+        allHeaderFinanceExp = []
+
+        for items in financialExp:
+            temp = items.h3.text  # Getting Header
+            temp2 = items.p.text # Getting Link
+            
+            
+            # Adding them in list.
+            allHeaderFinanceExp.append([temp])
+            allLinksFinanceExp.append([temp2])
+            
+        # We have two list. So, we will zip them. 
+        # First putting header in the zip then link.
+        linkAndHeader2 = zip(allHeaderFinanceExp,allLinksFinanceExp)
         
-    # return render(request, 'home.html',{'result': result})
-    # return render(request, 'home.html',{'divs': divs})
-    return render(request, 'home.html',{'linkAndHeader': linkAndHeader})
-
-
+    return render(request, 'home.html',{'linkAndHeader': linkAndHeader, 'linkAndHeader2': linkAndHeader2})
