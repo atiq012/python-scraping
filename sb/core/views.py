0867a76b7ec0d3aaaf2ec8e7dd4715bb3ba05dfb
atiq012 <atiqrshakil@gmail.com>
2021-09-30 12:25:43 +0600

    akashnews

--- sb/core/views.py
+++ sb/core/views.py
@@ -41,19 +41,40 @@ def get_html_content_new_age(keywords):
         html_content_newage = ''        
     
     return html_content_newage
+
+# jugantor
+def get_html_content_jugantor(keywords):
+    import requests
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+
+    if keywords:
+        keywords = keywords.replace(' ','+')
+        
+        html_content_jugantor = session.get(f'http://www.akashtv24.com/search?q={keywords}').text
+        
+    else:
+        keywords = ''
+        html_content_jugantor = ''        
     
+    return html_content_jugantor
+
+
 def home(request):
+    # datewise search
     # import datetime
     # stime = "09/21/2021 00:00:00.0000"
     
     # print(int(datetime.datetime.strptime(stime, '%m/%d/%Y %H:%M:%S.%f').timestamp()))
-    # 1632160800
-    # 
-    result = None
+    
     divs = None
     linkAndHeader = None
     linkAndHeader2 = None
-    
+    linkAndHeader3 = None
     if 'keywords' in request.GET:
         
         keywords = request.GET.get('keywords')
@@ -62,6 +83,9 @@ def home(request):
         
         # financial express
         html_content_new_age = get_html_content_new_age(keywords)
+
+        # jugantor news
+        html_content_jugantor = get_html_content_jugantor(keywords)
         
         import re
         from bs4 import BeautifulSoup
@@ -86,15 +110,37 @@ def home(request):
         # We have two list. So, we will zip them. 
         # First putting header in the zip then link.
         linkAndHeader = zip(allHeader, allLinks)
+
+        # jugantor news paper
+
+        soupForJugantor = BeautifulSoup(html_content_jugantor,'html.parser')
+        jugantor = soupForJugantor.find_all('div', class_ = "attachment-block")
+        allLinksAkash = []
+        allHeaderAkash = []
+        for items in jugantor:
+            
+            temp = items.h4.text  # Getting Header
+            
+            temp2 = items.a.get('href') # Getting Link
+            
+            
+            # Adding them in list.
+            allHeaderAkash.append([temp])
+            allLinksAkash.append([temp2])
+        linkAndHeader3 = zip(allHeaderAkash,allLinksAkash)
+
         
         # financial express
         soupForNewAge = BeautifulSoup(html_content_new_age,'html.parser')
         financialExp = soupForNewAge.find_all('div', class_="card row")
+        # financialExp = soupForNewAge.find_all('a', class_="local-news")
+
 
         allLinksFinanceExp = []
         allHeaderFinanceExp = []
 
         for items in financialExp:
+            
             temp = items.h3.text  # Getting Header
             temp2 = items.p.text # Getting Link
             
@@ -102,9 +148,8 @@ def home(request):
             # Adding them in list.
             allHeaderFinanceExp.append([temp])
             allLinksFinanceExp.append([temp2])
+        
             
-        # We have two list. So, we will zip them. 
-        # First putting header in the zip then link.
         linkAndHeader2 = zip(allHeaderFinanceExp,allLinksFinanceExp)
         
-    return render(request, 'home.html',{'linkAndHeader': linkAndHeader, 'linkAndHeader2': linkAndHeader2})
+    return render(request, 'home.html',{'linkAndHeader': linkAndHeader, 'linkAndHeader2': linkAndHeader2, 'linkAndHeader3':linkAndHeader3})
